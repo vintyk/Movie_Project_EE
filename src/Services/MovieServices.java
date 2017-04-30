ec1f485dc0b482ac129d7652d2d812b6f3d1efcd
vinty <vinty1978@gmail.com>
2017-04-30 23:11:34 +0300

    все jsp-шки

--- src/Services/MovieServices.java
+++ src/Services/MovieServices.java
@@ -1,11 +1,37 @@
 package Services;
 
+import dto.CreateMoviesDto;
+import video.Entity.Movies;
+import video.Entity.People;
 import video.dao.MoviesDao;
+import video.dao.PeopleDao;
 
 /**
  * Created by Vinty on 13.04.2017.
  */
 public class MovieServices {
+    private static final Object LOCK = new Object();
+    private static MovieServices INSTANCE = null;
 
+    private MovieServices() {
+    }
 
+    public static MovieServices getInstance() {
+        if (INSTANCE == null) {
+            synchronized (LOCK) {
+                if (INSTANCE == null) {
+                    INSTANCE = new MovieServices();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public void createNewMovie(CreateMoviesDto dto) {
+        MoviesDao.getInstance().create(new Movies(
+                dto.getNameMovies(),
+                dto.getDateMovies()),
+                dto.getGenreMovies(),
+                dto.getCountrieMovies());
+    }
 }
